6e7fbf441450c47579f5d5377efe92493e0bf354
Juwon <dnladesanya1@gmail.com>
2023-01-07 12:19:56 +0100

    join tables

--- schema.sql
+++ schema.sql
@@ -8,9 +8,19 @@ CREATE TABLE owners( id SERIAL NOT NULL PRIMARY KEY, full_name VARCHAR, age inte
 
 CREATE TABLE species( id SERIAL NOT NULL PRIMARY KEY, name VARCHAR);
 
-ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals
+
+DROP COLUMN species;
 
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+--vets TABLE
 
+CREATE TABLE vets (id SERIAL NOT NULL  PRIMARY KEY, name VARCHAR, age INT, date_of_graduation DATE);
+-- specialization TABLE
+
+CREATE TABLE specializations( species_id INT REFERENCES species(id), vet_id INT REFERENCES vets(id));
+-- visits TABLE
+
+CREATE TABLE visits( animal_id INT REFERENCES animals(id), vet_id INT REFERENCES vets(id), date_of_visit DATE);
