ab3dafeb821769ebf0a2189a59c197031910f9e7
Juwon <dnladesanya1@gmail.com>
2023-01-18 16:41:48 +0100

    git pulled into dev

--- schema.sql
+++ schema.sql
@@ -23,4 +23,4 @@ CREATE TABLE vets (id SERIAL NOT NULL  PRIMARY KEY, name VARCHAR, age INT, date_
 CREATE TABLE specializations( species_id INT REFERENCES species(id), vet_id INT REFERENCES vets(id));
 -- visits TABLE
 
-CREATE TABLE visits( animal_id INT REFERENCES animals(id), vet_id INT REFERENCES vets(id), date_of_visit DATE);
+CREATE TABLE visits( animal_id INT REFERENCES animals(id), vet_id INT REFERENCES vets(id), date_of_visit DATE);`
